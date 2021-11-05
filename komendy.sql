c803e99f5044568e2d1fed5d34872775a6204911
Kolszewski01 <lemkon24@gmail.com>
2021-11-05 15:15:03 +0100

    dodanie zadan z lab4

--- komendy.sql
+++ komendy.sql
@@ -68,8 +68,95 @@ insert into przetwory values
 # Zadanie 5
 	#1. 
 insert into postac values
-(default, 'Thor', 'wiking', '1971-07-18', '54'),
-(default, 'Gil', 'wiking', '2005-02-07', '2'),
-(default, '', '', '', '',),
-(),
-();
+(default, 'Thor', 'wiking', '1971-07-18', 54),
+(default, 'Gil', 'wiking', '2005-02-07', 20),
+(default, 'Michal', 'wiking', '2001-01-08', 24),
+(default, 'Frode', 'wiking', '1968-12-04', 57),
+(default, 'Ziggy', 'wiking', '1995-07-09', 30);
+	#2.
+create table statek (
+	nazwa_statku varchar(40) primary key,
+	rodzaj_statku enum('great_ship','Skeid','Busse'),
+	data_wodowania date,
+	max_ladownosc int unsigned
+);
+	#3.
+insert into statek values
+('Boar','great_ship','1984-04-09',25),
+('Raven','Skeid','2000-03-24',12);
+	#4.
+alter table postac add column funkcja varchar(40);
+	#5.
+update postac set funkcja='kapitan' where nazwa='Bjorn';
+	#6.
+alter table postac add column postac_statek varchar(40);
+alter table postac add CONSTRAINT 'postac_ibfk_1' foreign key (postac_statek) references statek(nazwa_statku);
+	#7.
+update postac set postac_statek='Boar' where id_postaci=1;
+update postac set postac_statek='Boar' where id_postaci=2;
+update postac set postac_statek='Boar' where id_postaci=6;
+update postac set postac_statek='Boar' where id_postaci=8;
+update postac set postac_statek='Raven' where id_postaci=4;
+update postac set postac_statek='Raven' where id_postaci=5;
+update postac set postac_statek='Raven' where id_postaci=7;
+	#8.
+delete from izba where nazwa_izby='spizarnia';
+	#9.
+drop table izba;
+
+
+#lab 04
+# Zadanie 1
+	#a) 
+#select nazwa, wiek from postac order by wiek DESC;
+delete from postac where nazwa = 'Frode';
+delete from postac where nazwa = 'Thor';
+	#b)
+alter table postac modify id_postaci int not null;
+# [alternatywnie] wyłączenie sprawdzania poprawności kluczy obcych w danej bazie
+#SET foreign_key_checks = 0;
+#usuwanie klucza obcego
+#ALTER TABLE walizka/przetwory/itd. DROP FOREIGN KEY ..._ibfk_...;
+ALTER TABLE postac DROP FOREIGN KEY postac_ibfk_1;
+#możemy spróbować usunąć klucz główny
+#ALTER TABLE postac DROP Primary key;
+# Zadanie 2
+	#a)
+alter table postac add column pesel char(11) primary key;
+	#b)
+alter table postac modify rodzaj enum('wiking','ptak','kobieta','syrena');
+insert into postac 
+	(default,'Gertruda Nieszczera','syrena','1974-04-26',51);
+# Zadanie 3
+	#a)
+#SELECT nazwa from postac WHERE nazwa LIKE '%a%';
+update postac set postac_statek='Boar' where nazwa like '%a%';
+	#b)
+update statek set max_ladownosc=max_ladownosc*0.7 where data_wodowania between '1901-01-01' AND '2000-12-31';
+	#c)
+alter table postac add check (wiek<1000);
+# Zadanie 4
+	#a)
+insert into postac values
+	(default,'waz Loko','1525-03-04','500',null,null);
+	#b)
+#create table marynarz like postac;
+create table marynarz select * from postac where postac_statek is not null;
+	#c)
+	td
+# Zadanie 5
+	#a)
+update postac set postac_statek=null;
+	#b)
+delete from postac where nazwa='Gil';
+	#c)
+delete from statek;
+	#d)
+drop table statek;
+	#e)
+create table zwierz(
+	id int primary key auto_increment,
+	nazwa varchar(40),
+	wiek int);
+	#f)
+insert into zwierz select id_postaci, nazwa, wiek from postac;
