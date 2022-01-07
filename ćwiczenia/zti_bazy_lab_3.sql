3f540b618f979493a68fbe7bd83d2b241028bd10
Dawid <jakismail@gmail.com>
2022-01-07 15:53:24 +0100

    zti_bazy_lab_3 zdania 2

--- ćwiczenia/zti_bazy_lab_3.sql
+++ ćwiczenia/zti_bazy_lab_3.sql
@@ -37,12 +37,49 @@ inner join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
 group by p.id_pracownika order by sum(pz.ilosc*pz.cena) DESC;
 
 	#lab_3_zadania_2.md
-	#1 
+	#1 +
 Select d.nazwa, min(p.pensja), max(p.pensja), avg(p.pensja) from dzial d
 left join pracownik p on d.id_dzialu=p.dzial
 group by d.nazwa;
-	#2
-Select k.pelna_nazwa, sum(pz.ilosc*pz.cena) from klient k
-inner join zamowienie z on k.id_klienta=z.klient
-inner join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
-group by id_klienta order by sum(pz.ilosc*pz.cena) DESC limit 10;
+	#2 -
+Select k.pelna_nazwa, pz.ilosc*pz.cena from klient k
+left join zamowienie z on k.id_klienta=z.klient
+left join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
+order by pz.ilosc*pz.cena DESC limit 10;
+	#3 +
+SELECT sum(pz.cena*pz.ilosc), year(z.data_zamowienia) from zamowienie z
+left join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
+group by year(z.data_zamowienia) order by sum(pz.cena*pz.ilosc) DESC;
+	#4 +
+SELECT sum(pz.ilosc*pz.cena) from zamowienie z
+left join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
+left join status_zamowienia sz on z.status_zamowienia=sz.id_statusu_zamowienia
+where sz.nazwa_statusu_zamowienia='anulowane';
+	#5 -
+select ak.miejscowosc, count(z.id_zamowienia), sum(pz.ilosc*pz.cena) from pozycja_zamowienia pz
+left join zamowienie z on pz.zamowienie=z.id_zamowienia
+left join klient k on z.klient=k.id_klienta
+left join adres_klienta ak on k.id_klienta=ak.klient
+group by ak.miejscowosc;
+	#6 +
+SELECT sum((pz.cena-t.cena_zakupu)*pz.ilosc) from zamowienie z
+left join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
+left join status_zamowienia sz on z.status_zamowienia=sz.id_statusu_zamowienia
+left join towar t on pz.towar=t.id_towaru
+where sz.nazwa_statusu_zamowienia='zrealizowane';
+	#7 +
+SELECT sum((pz.cena-t.cena_zakupu)*pz.ilosc) from zamowienie z
+left join pozycja_zamowienia pz on z.id_zamowienia=pz.zamowienie
+left join status_zamowienia sz on z.status_zamowienia=sz.id_statusu_zamowienia
+left join towar t on pz.towar=t.id_towaru
+left join stan_magazynowy sm on t.id_towaru=sm.towar
+where sz.nazwa_statusu_zamowienia='zrealizowane'
+group by year(z.data_zamowienia);
+	#8 +
+select sum(t.cena_zakupu*sm.ilosc), k.nazwa_kategori from towar t
+left join stan_magazynowy sm on t.id_towaru=sm.towar
+left join kategoria k on t.kategoria=k.id_kategori
+group by k.nazwa_kategori;
+	#9 - SELECT MONTHNAME(CURDATE()); 
+select MONTHNAME(data_urodzenia) from pracownik
+group by month(data_urodzenia);
